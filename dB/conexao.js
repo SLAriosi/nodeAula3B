d9dcfbff8b5801e3b3aaccca9f033ea892b4475b
UniALFA <unialfa@MACMINI-14.local>
2024-03-11 20:05:27 -0300

    fixing the code

--- dB/conexao.js
+++ dB/conexao.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize ({
     dialect: 'sqlite',
-    storage: './db/app.db'
+    storage: './dB/app.dB'
 })
 
 module.exports = sequelize
