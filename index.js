d0387dcb8e5410ec5c56d5b962efc1c51843f948
UniALFA <unialfa@MACMINI-14.local>
2024-03-11 20:23:42 -0300

    testando

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const dB = require('./dB/conexao')
+const Sequelize = require('sequelize')
+
+dB.authenticate()
+    .then(() => {
+        console.log('Conectou ao Banco de Dados')
+    })
+    .catch((erro) => {
+        console.log(`Erro ao tentar conectar no DB ${erro}`)
+    })
 
 const PORT = 3001
 
 const app = express()
-
 
 app.use('/jobs', require('./routes/jobs'))
 
